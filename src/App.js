da1c85acd836546ddf13e37d57b1066be33c0d8c
CollinDex <obetscollins@gmail.com>
2023-03-18 14:59:05 +0100

    Added Routing Functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ParticlesBg from 'particles-bg';
 import Navigation from "./components/Navigation/Navigation";
+import SignIn from "./components/SignIn/SignIn";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
@@ -49,15 +50,29 @@ class App extends Component {
       input: '',
       imageUrl: '',
       faceCount: '',
-      box: {}
+      box: {},
+      route: 'signin'
     }
   }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const image = document.getElementById('inputImage');
     const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    console.log(data.outputs[0].data.regions[0].region_info.bounding_box);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
 
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -67,11 +82,12 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input}); //Set stata for the image-link sent to the API
 
-    fetch('https://api.clarifai.com/v2/models/face-detection/outputs', returnClarifaiJSONRequest(this.state.input))
+    fetch('https://api.clarifai.com/v2/models/face-detection/outputs',
+    returnClarifaiJSONRequest(this.state.input))
     .then(response => response.json())
     .then(response => {
       this.setState({faceCount: response.outputs[0].data.regions.length}); //Sets the No of Detected Faces 
-      this.calculateFaceLocation(response); //Calls the FaceLoocation Calculator
+      this.displayFaceBox(this.calculateFaceLocation(response)); //Calls the FaceLocation Calculator
 
       /* if (response) {
         fetch('http://localhost:3000/image', {
@@ -91,21 +107,32 @@ class App extends Component {
     .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
   render() { //Render App Components
     return (
       <div className="App">
         <ParticlesBg color="#EEEEEE" num={150} type="cobweb" bg={true}/>
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition 
-          imageUrl={this.state.imageUrl}
-          faceCount={`Detected ${this.state.faceCount} Face(s)`}
-        />
+        <Navigation onRouteChange={this.onRouteChange}/>
+        {this.state.route === 'signin'
+          ? <SignIn onRouteChange={this.onRouteChange}/>
+          : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition 
+                box={this.state.box}
+                imageUrl={this.state.imageUrl}
+                faceCount={`Detected ${this.state.faceCount} Face(s)`}
+              />
+            </div>
+        }
+        
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,12 @@
 import React from "react";
 import '../../App.css'
-
-const FaceRecognition = ({ imageUrl, faceCount }) => {
+import './FaceRecognition.css'
+const FaceRecognition = ({ imageUrl, faceCount, box }) => {
     return(
         <div className="center ma">
             <div className="absolute mt2">
-                <img id='inputimage' src={imageUrl} alt="" width='500px' height='auto'/>
+                <img id='inputImage' src={imageUrl} alt="" width='500px' height='auto'/>
+                <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
                 <p className="white f3">{faceCount}</p>
             </div>
             
